a1a65f4b65912d3649dc0b71059edc7106f2f59b
nicoundurraga <nicolas@pingon.cl>
2018-07-24 16:22:42 -0400

    Initial commit

---
+++ app/src/main/java/cl/pingon/cotizador/LoginActivity.java
@@ -0,0 +1,73 @@
+package cl.pingon.cotizador;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+
+
+public class LoginActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 007;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+
+        if (new CurrentUser().getCurrentUser() != null) {
+            logged();
+        } else {
+            signUp();
+        }
+
+
+    }
+
+    private void signUp() {
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.EmailBuilder().build(),
+                                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                new AuthUI.IdpConfig.FacebookBuilder().build()))
+                        .build(),
+                RC_SIGN_IN);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (RC_SIGN_IN == requestCode) {
+
+            if (RESULT_OK == resultCode) ;
+            {
+                logged();
+            }
+        }
+    }
+
+    private void logged() {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+
+}
