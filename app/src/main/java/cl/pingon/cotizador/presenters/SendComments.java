81e794275bc9f91d11152af2e861315d118cb786
nicoundurraga <nicolas@pingon.cl>
2018-11-08 14:32:00 -0300

    final

--- app/src/main/java/cl/pingon/cotizador/presenters/SendComments.java
+++ app/src/main/java/cl/pingon/cotizador/presenters/SendComments.java
@@ -5,13 +5,8 @@ import cl.pingon.cotizador.model.MachinesDetails;
 
 public class SendComments {
 
-    private MachinesDetails machinesDetails = new MachinesDetails();
 
     public void fromUser(String key, String comment) {
-        if (comment.trim().length() > 0) {
-            machinesDetails.setComments(comment);
-
-        }
 
         new Nodes().machines(key).child("comments").setValue(key, comment);
 
